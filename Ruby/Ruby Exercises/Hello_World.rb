d30f49fe9eb1846b6b3edf9eb4760222ed0504c9
cristian-santiago <103728899+cristian-santiago@users.noreply.github.com>
2022-05-16 22:22:30 -0300

    Delete Hello_World.rb

--- Ruby/Ruby Exercises/Hello_World.rb
+++
@@ -1,9 +0,0 @@
-# there are two ways to print a msg on bash 
-
-
-# The Print way all the msgs will be printed in sequence
-print "Hello World" 
-print ", this is my first command in Ruby."
-print " Now the 'Hello, World' curse won't affect me."
-
-#
