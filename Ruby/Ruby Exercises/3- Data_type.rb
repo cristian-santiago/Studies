460d336ccc48800f6aa85308eb3cacaf862f1e34
cristian-santiago <103728899+cristian-santiago@users.noreply.github.com>
2022-05-17 22:31:52 -0300

    Create 3- Data_type.rb

---
+++ Ruby/Ruby Exercises/3- Data_type.rb
@@ -0,0 +1,7 @@
+# Different variables with othe data types
+
+name = 'Cristian' # String variable, 
+age = 30 # Number variable (also can include floating)
+ismale = true # Boolean variables
+istall = false # Boolean variables
+hobbies = nil # This variable hasn't a value
