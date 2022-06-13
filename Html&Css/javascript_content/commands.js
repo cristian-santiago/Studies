8f40713fd6b39189cca08c366e509b2bbd77d58b
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-06-13 04:30:05 -0300

    Create commands.js

---
+++ Html&Css/javascript_content/commands.js
@@ -0,0 +1,10 @@
+// Variables
+
+var nome  = "Cristian Santiago da Silva";
+var idade = 30;
+var city = "Nova Iguaçu";
+
+console.log(nome); // add the name on the console log on browser
+console.log(nome.replace("da Silva", "Carvalho")); // changes the strings from var name to another one
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
