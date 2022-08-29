1661455caa8e1969ea00143930d85ca75c36f258
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-08-29 00:39:12 -0300

    updating iterators loop with selector method

--- Ruby/23- iterators_loops.rb
+++ Ruby/23- iterators_loops.rb
@@ -27,7 +27,7 @@ fruits.each {|f| p f} # One line iterator method, used basically with small bloc
  end'''
 
  # Nested iterators: 
-
+'''
  teams = {
 
         "Real Madrid": {
@@ -53,3 +53,23 @@ fruits.each {|f| p f} # One line iterator method, used basically with small bloc
         p "#{player} holds the #{position} position."
     end
  end
+
+ '''
+
+ # Select Method
+
+ p (1..10).to_a.select(&:even?)
+
+ p (1..10).to_a.select { |x| x.even? }
+
+ p((1..10).to_a.select do |x|
+    x.even?
+ end)
+
+ words = %w(The select method now will only return the words that are over five letters)
+
+p words.select { |x| x.length > 5}
+
+
+# Given an array of strings and it will return all of the vawels (It includes regex)
+p %w(a b c d e f g).select { |v| v=~ /[aeiou]/}
