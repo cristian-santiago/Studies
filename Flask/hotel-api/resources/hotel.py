5492e448df182d057c812886b51b413c61a21d78
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2023-01-09 23:17:01 -0300

    updating the query filter api

--- Flask/hotel-api/resources/hotel.py
+++ Flask/hotel-api/resources/hotel.py
@@ -1,11 +1,85 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+
+
+def normalize_path_params(
+    cidade=None,
+    estrelas_min = 0,
+    estrelas_max = 5,
+    diaria_min = 0,
+    diaria_max = 10000,
+    limit = 50,
+    offset = 0, **dados):
+
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min' : diaria_min,
+            'diaria_max' : diaria_max,
+            'cidade': cidade,
+            'limit' : limit,
+            'offset' : offset
+        }
+    
+    
+    return {
+        'estrelas_min': estrelas_min,
+        'estrelas_max': estrelas_max,
+        'diaria_min' : diaria_min,
+        'diaria_max' : diaria_max,        
+        'limit' : limit,
+        'offset' : offset
+    }
+
+
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estrelas_min', type=float)
+path_params.add_argument('estrelas_max', type=float)
+path_params.add_argument('diaria_min', type=float)
+path_params.add_argument('diaria_max0', type=float )
+path_params.add_argument('limit',type=float)
+path_params.add_argument('offset', type=float)
 
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} #List comprehension
+
+        connection = sqlite3.connect('hotel.db')
+        cursor = connection.cursor()      
+
+
+        dados = path_params.parse_args()
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None} #received only the valida data from path_params
+        parametros = normalize_path_params(**dados_validos)
+
+        if not parametros.get('cidade'):
+            consulta = """
+            SELECT * FROM hoteis
+            WHERE (estrelas > ? and estrelas < ?)
+            AND (diaria > ? and diaria < ?)
+            LIMIT ? OFFSET ?            
+            """
+            tupla = tupla([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+        else:
+            consulta = """
+            SELECT * FROM hoteis
+            WHERE (estrelas > ? and estrelas < ?)
+            AND (diaria > ? and diaria < ?)
+            AND cidade = ? LIMIT ? OFFSET ?            
+            """
+            tupla = tupla([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+        hoteis = []
+        for i in resultado:
+            hoteis.append()
+           
+        #return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} #List comprehension
 
 class Hotel(Resource):
 
